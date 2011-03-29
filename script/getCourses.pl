a36fa9b88d1d5f2a675fc3cab0311b348e39a2a7
Reed La Botz <reedlabotz@gmail.com>
2011-03-29 12:23:56 -0500

    Created script to scrape courses off of illinois web site.

---
+++ script/getCourses.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use URI;
+use Web::Scraper;
+
+open FILE, ">courses.sql" or die $!;
+
+
+# website to scrape
+my $urlToScrape = "http://courses.illinois.edu/cis/2011/spring/catalog/index.html";
+
+# prepare data
+my $teamsdata = scraper {
+ # we will save the urls from the teams
+ process "div.ws-subject-row div.ws-course-title a", 'urls[]' => '@href';
+ # we will save the team names
+ process "div.ws-subject-row div.ws-course-title a", 'names[]' => 'TEXT';
+};
+# scrape the data
+my $res = $teamsdata->scrape(URI->new($urlToScrape));
+
+# print the second field (the teamname)
+for my $i (0 .. $#{$res->{names}}) {
+ print "Grabbing " . $res->{names}[$i];
+ print "\n";
+ 
+ # prepare data
+ my $teamsdata = scraper {
+  # we will save the urls from the teams
+  process "div.ws-row div.ws-course-number", 'numbers[]' => 'TEXT';
+  # we will save the team names
+  process "div.ws-row div.ws-course-title a", 'titles[]' => 'TEXT';
+ };
+ # scrape the data
+ my $res = $teamsdata->scrape(URI->new($res->{urls}[$i]));
+
+ # print the second field (the teamname)
+ for my $i (0 .. $#{$res->{numbers}}) {
+    my $dep = "";
+    my $num = "";
+   ($dep,$num) = split(/[\ \t\n]+/,$res->{numbers}[$i]);
+  my $name = $res->{titles}[$i];
+  print "$dep $num - $name";
+  print "\n";
+  
+  
+  # put the data in a file
+  print FILE "INSERT INTO courses (department,number,name) VALUES (\"$dep\",\"$num,\",\"$name\");";
+  print FILE "\n";
+  
+ }
+ print "-----------------------------------------------------------\n";
+ sleep(3);
+}
+
+
+close FILE;
